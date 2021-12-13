8982c91b10f481861a093b014d9468e41b6428c5
anand1115 <anandpasam1115@gmail.com>
2021-12-13 10:38:34 +0530

    update

--- MedMarket/settings.py
+++ MedMarket/settings.py
@@ -170,9 +170,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL="/media/"
+
+MEDIA_ROOT=os.path.join(BASE_DIR,"media")
+
+import mimetypes
+
+mimetypes.add_type("text/css", ".css", True)
