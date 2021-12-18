a84aff0e12f3a9e51cfe5d49b973b376614bc16b
anand1115 <anandpasam1115@gmail.com>
2021-12-18 14:37:09 +0530

    update

--- MedMarket/settings.py
+++ MedMarket/settings.py
@@ -181,12 +181,19 @@ import os
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+if PRODUCTION:
+    STATIC_URL = '/api/static/'
+else:
+    STATIC_URL = '/static/'
 
-STATIC_URL = '/static/'
 
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+if PRODUCTION:
+    MEDIA_URL="/api/media/"
+else:
+    MEDIA_URL="/media/"
 
-MEDIA_URL="/media/"
+
 
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 
