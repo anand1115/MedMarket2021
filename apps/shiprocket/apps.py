ef4bdae816e036951ca793bd5742a9334499a731
anand1115 <anandpasam1115@gmail.com>
2022-01-11 11:28:35 +0530

    update

--- apps/shiprocket/apps.py
+++ apps/shiprocket/apps.py
@@ -5,6 +5,6 @@ class ShiprocketConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.shiprocket'
 
-    # def ready(self):
-    #     from . import scheduler
-    #     scheduler.start()
+    def ready(self):
+        from . import scheduler
+        scheduler.start()
